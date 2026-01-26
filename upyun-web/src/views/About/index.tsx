830b3180b33c5fdcb85608a400d91edd539b6e73
小沐沐吖 <sunnyboy_mu@163.com>
2026-01-26 20:26:15 +0800

    feat: 添加 Tailwind CSS 支持并配置路径别名

--- upyun-web/src/views/About/index.tsx
+++ upyun-web/src/views/About/index.tsx
@@ -3,7 +3,9 @@ import { NavLink } from "react-router";
 export default function About() {
   return (
     <div>
-      <NavLink to="/">Home</NavLink>
+      <NavLink className="text-yellow-200 " to="/">
+        Home
+      </NavLink>
     </div>
   );
 }
--- upyun-web/vite.config.ts
+++ upyun-web/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,9 +11,12 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
   server: {
-    // 配置允许访问的主机列表
-    // 可选：如果需要开放所有主机访问（开发环境临时使用）
     allowedHosts: true,
   },
 });
